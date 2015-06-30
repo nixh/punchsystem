4983b1ccc1b7992f3ea358bab2bf32178712e8f7
yongred <yongding@live.com>
2015-06-30 17:21:02 -0400

    login-staffMain html
    implemented basic layout for login and staff_main page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', function(req, res, next){
+	
+	res.render('test/prac', { name:"Yong D Liu"});
+});
+
+router.get('/login', function(req, res, next){
+	
+	res.render('login');
+});
+
+router.get('/staff_main', function(req, res, next){
+	
+	res.render('staff_main', {
+		users: {
+			company_userid : "company_userid",
+			name : "name",
+			createDate: "Date",
+			password: "password", // Encrypted Text
+			sex : true,
+			email : 1231425
+		}
+	});
+});
+
+
 module.exports = router;
