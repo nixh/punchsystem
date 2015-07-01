ba3bae1f5904ea627bfee46a81915defa95a19ef
ReimiFans <saiqiuli@gmail.com>
2015-07-01 12:11:53 -0400

    qiuli first commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var monk = require('monk');
+var db = monk('mongodb://localhost:27017/punchtest');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var test = db.get('test');
+    var results = test.find({}, {});
+    res.render('index', { title: 'Express', ret: results });
+});
+
+router.get('/test/:name', function(req, res, next) {
+    var col = db.get(req.params.name);
+    col.find({}, {limit:20}, function(e, docs){
+        res.json(docs);
+    });
+
 });
 
 module.exports = router;
---
+++ utils.js
@@ -0,0 +1,16 @@
+function Utils() {
+
+    return {
+        render : function(tmplPath, data) {
+            return function(req, res, next) {
+                if(!data) data = {};
+                data['tr'] = res.__;
+                return res.render(tmplPath, data);
+            }
+        }
+    }
+}
+
+
+module.exports = new Utils();
+
