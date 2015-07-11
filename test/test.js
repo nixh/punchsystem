7e22c23f67924bf952fcaac7a90970c3760c8385
dsj77222 <sd2411@nyu.edu>
2015-07-11 10:03:11 -0400

    dsj

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 var assert = require ("assert");
@@ -150,22 +149,4 @@ describe ("Module",function(){
 
 
 
-=======
-var assert = require('assert');
-var usermodule = require('../usermodule');
 
-var um = new usermodule();
-
-describe('usermodule', function(){
-    describe('#addUser()', function(){
-        it('shoud insert a user into the database', function(done){
-            var userObj = {
-                userid: 'romanchelsea',
-                password: 'satosumire'
-            };
-
-            um.addUser(userObj, )
-        })
-    })
-})
->>>>>>> dev
