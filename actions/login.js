36367c8e1ba3fb25a98873d41b08fb02491bc24a
ReimiFans <saiqiuli@gmail.com>
2015-07-28 17:42:51 -0400

    change db add flatten

--- actions/login.js
+++ actions/login.js
@@ -1,7 +1,8 @@
 var utils = require('../lib/common/utils');
 var actionUtils = utils.actions;
-var auth = require('../lib/module/authModule');
+var AuthModule = require('../lib/module/authModule');
 var Q = require('q');
+var auth = new AuthModule();
 
 login = {};
 
@@ -10,8 +11,12 @@ login.view = actionUtils.jadeAction('login');
 login.auth = {
     type: 'redirect',
     execute: function(req, res, next) {
-        return Q("/staff_main");
+        auth.login('user1', '123123').then(function(valid){
+            console.log(valid);
+        })
+        
     }
 }
 
 module.exports = login;
+
