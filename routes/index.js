716d7f2f015ba9d51c78358c50c34b61da425e21
yongred <yongding@live.com>
2015-07-29 15:21:24 -0400

    punch record edit

--- routes/index.js
+++ routes/index.js
@@ -212,7 +212,7 @@ router.get('/punch/:key', function(req, res, next) {
 
 });
 
-
+/*
 var qrModule = require('../qrcodeModule');
 
 router.get('/supervisor/showdynacode', function(req, res, next) {
@@ -225,7 +225,7 @@ router.get('/supervisor/showdynacode', function(req, res, next) {
         })(req, res, next);
     });
 });
-
+*/
 
 router.get('/recentRecords', function(req, res, next) {
     var rm = new recordsModule();
