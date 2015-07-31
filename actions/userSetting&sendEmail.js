269a90a58f3650359981481856c78d56f1064fac
dsj77222 <sd2411@nyu.edu>
2015-07-31 18:25:02 -0400

    dsj

--- actions/userSetting&sendEmail.js
+++ actions/userSetting&sendEmail.js
@@ -6,13 +6,35 @@ var Q = require('q');
 
 userSettings ={};
 
+Settings.emailSwitch = {
+	type : 'jade',
+	template : './staff/staff_setting_su',
+	execute: function(req,res,next) {
+		var userid = req.params.userid;
+		var switchs = parseInt(req.params.switchs);
+		if(req.params.switchs == 1) {
+			switchs = 0;
+		}
+		else {
+			switchs = 1;
+		}
+		var docs = s.enableEmailById(sessionid,switchs);
+		return docs.then(function (doc) {
+			var data = {
+				'status' : 'success'
+			};
+			return data;
+		});
+	}	
+}
+
 userSettings.changePass = {
 	type : 'api',
 	execute: function(req,res,next) {
-		var sessionid = req.cookies.sessionid;
+		var userid = req.params.userid;
 		var oldpass = req.body.oldpass;
 		var newpass = req.body.newpass;
-		var docs = s.changepass(oldpass,newpass,sessionid)
+		var docs = s.changepassById(oldpass,newpass,sessionid)
 		return docs.then(function (doc) {
 			
 			if(!doc) {
@@ -34,7 +56,7 @@ userSettings.changePass = {
 userSettings.sendEmail = {
 	type : "api",
 	execute: function(req,res,next) {
-		var sessionid = req.cookies.sessionid;
+		var userid = req.params.userid;
 		var cc = '';
 		var subject = 'Rate report';
 		var html = 'Rate report<i>this month </i>.';
