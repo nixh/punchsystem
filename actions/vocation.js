4b299140b0f1859245d86f9d019d2c58335e6409
ReimiFans <saiqiuli@gmail.com>
2015-08-17 12:19:30 -0400

    vocation and user

--- actions/vocation.js
+++ actions/vocation.js
@@ -5,9 +5,18 @@ var config = require('../lib/common/config')();
 var moment = require('moment');
 var _ = require('underscore');
 
-var vocation = {}
+var vocation = {};
 
 var vm = factory.get('vocationModule');
+
+vocation.newVocation = {
+    type: 'api',
+    execute: function(req, res, next) {
+        var userid = req.body.userid;
+        if(!userid) throw new Error('lack params.');
+
+    }
+};
 
 vocation.checkVocation = {
     type: 'api',
@@ -15,19 +24,23 @@ vocation.checkVocation = {
         var status = req.params.status;
         if(status) {
             var availableStatus = ['pending', 'approved', 'rejected'];
-            var any = _.any(availableStatus, function(st){ st === status });
+            var any = _.any(availableStatus, function(st){ return st === status; });
             if(!any)
-                throw new Error('incorrect status.')
+                throw new Error('incorrect status.');
         }
         var userid = req.params.userid;
         if(!userid) throw new Error('lack params.');
-        return vm.checkVocations(userid, status)
+        return vm.checkVocations(userid, status);
     }
-}
+};
 
 vocation.cancelVocation = {
     type: 'api',
     execute: function(req, res, next) {
-
+        var userid = req.body.userid;
+        if(!userid) throw new Error('lack params.');
+        var vocation_id = req.body.vocationid;
+        if(!vocation_id) throw new Error('lack params.');
+        return vm.cancelVocation(userid, vocation_id);
     }
-}
+};
